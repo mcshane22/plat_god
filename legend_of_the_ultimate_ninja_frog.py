1c560057b35b0fd66a7dc3c5cd5ae82149aa724c
mcshane22 <157163175+mcshane22@users.noreply.github.com>
2024-05-20 18:38:44 +0000

    setup

---
+++ legend_of_the_ultimate_ninja_frog.py
@@ -0,0 +1,26 @@
+import pygame
+
+pygame.init()
+
+WIDTH, HEIGHT = 800, 600
+FPS = 60
+
+pygame.display.set_caption("LEGEND OF NINJA FROG")
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main(window):
+     clock = pygame.time.Clock()
+
+     running = True
+     while running:
+          clock.tick(FPS)
+
+          for event in pygame.event.get():
+               if event.type == pygame.QUIT:
+                    running = False
+                    break
+     pygame.QUIT()
+     quit()
+
+if __name__ == "__main__":
+     main(window)
